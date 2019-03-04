8857bf8b578aabdd160ea6b53d1d09ada5864594
MegatronKing <1256980529@qq.com>
2019-03-04 23:22:35 +0800

    init commit

---
+++ app/src/main/java/com/github/megatronking/stringfog/sample1/TestIngore.java
@@ -0,0 +1,15 @@
+package com.github.megatronking.stringfog.sample1;
+
+import com.github.megatronking.stringfog.annotation.StringFogIgnore;
+
+/**
+ * Test StringFogIgnore
+ */
+@StringFogIgnore
+public class TestIngore {
+
+    public static final String A = "a";
+    public static final String B = "b";
+    public static final String C = "c";
+
+}
